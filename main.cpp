168aa7ffe4f9b227d576d9d4935416fff2b5469d
oseq <osek95@gmail.com>
2017-05-10 23:41:26 +0200

    testowy commit 2

--- main.cpp
+++ main.cpp
@@ -13,7 +13,13 @@ int main() {
 
     cout << "Jak sie nazywa twoja dupa?" << endl;
     cin>>wyraz;
-    cout<<"Twoja "<<wyraz<<" kocha cie na "<<los<<"%";
+    cout<<"Twoja "<<wyraz<<" kocha cie na "<<los<<"%"<<endl;
+
+    //jakies zmiany xd
+
+    //STACJA BENZYNOWA
+
+    cout<<"Stacja beznynowa XDDD";
 
 
     return 0;
